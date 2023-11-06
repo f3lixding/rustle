998621377a6fd5caf97b0aa202acc8cf8ac0bd51
Felix Ding <felixding1989@gmail.com>
2023-11-05 19:20:49 -0800

    Adds port reuse to both main and sub socket

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::net::SocketAddr;
-use std::sync::atomic::{AtomicI32, Ordering};
 use std::sync::Arc;
 use tokio::net::UdpSocket;
 use tokio::sync::RwLock;
@@ -10,7 +9,8 @@ mod query_service;
 
 pub use query_service::{QueryService, Ready, Response};
 
-type LongRunningTaskType = JoinHandle<Result<(), Box<dyn std::error::Error + Send + Sync>>>;
+type OpaqueError = Box<dyn std::error::Error + Send + Sync>;
+type LongRunningTaskType = JoinHandle<Result<(), OpaqueError>>;
 
 /// Provider for task that listens for external messages.
 /// Upon receiving a message (which would be a UDP packet, because it's a DNS query), it spawns a
@@ -18,42 +18,32 @@ type LongRunningTaskType = JoinHandle<Result<(), Box<dyn std::error::Error + Sen
 ///
 /// Currently there is a maximum queue size.
 pub async fn get_input_tasks(
-    max_queue_size: i32,
     socket_orig_sender: Arc<UdpSocket>,
-    router_addr: String,
-    query_service: QueryService<Ready>,
-) -> Result<(LongRunningTaskType, LongRunningTaskType), Box<dyn std::error::Error + Send + Sync>> {
+    socket_subrequest: Arc<UdpSocket>,
+    router_addr: &str,
+    query_service: Arc<QueryService<Ready>>,
+) -> Result<(LongRunningTaskType, LongRunningTaskType), OpaqueError> {
     // 4 kbytes of buffer allocated.
     // This should be fine since historically DNS queries operated within the UDP packet limit,
     // which is only 512 bytes
     let mut buf = [0; 1024];
-    let current_queue_size = Arc::new(AtomicI32::new(0));
     let parking_lot: Arc<RwLock<HashMap<u16, SocketAddr>>> = Arc::new(RwLock::new(HashMap::new()));
     let parking_lot_clone = parking_lot.clone();
     // We'll use a different socket for now for subrequest. Technically we do not need to do that.
-    let socket_subrequest = Arc::new(UdpSocket::bind("[::]:0").await?);
     let socket_subrequest_clone = socket_subrequest.clone();
 
+    let router_addr = Arc::new(router_addr.to_string());
     let main_listener_task = tokio::spawn(async move {
         let query_service = Arc::new(query_service);
-        let router_addr = Arc::new(router_addr);
         loop {
             let (size, addr) = socket_orig_sender.recv_from(&mut buf).await?;
             let content = buf[..size].to_vec();
-
-            let current_queue_size = current_queue_size.clone();
-
-            if current_queue_size.load(Ordering::Relaxed) >= max_queue_size {
-                println!("queue full please try again later");
-                continue;
-            }
 
             let socket_subrequest = socket_subrequest_clone.clone();
             let parking_lot = parking_lot_clone.clone();
             let query_service = query_service.clone();
             let socket_orig_sender = socket_orig_sender.clone();
             let router_addr = router_addr.clone();
-            current_queue_size.fetch_add(1, Ordering::Relaxed);
 
             tokio::spawn(async move {
                 // TODO: log this
@@ -72,13 +62,12 @@ pub async fn get_input_tasks(
                     }
                 }
 
-                current_queue_size.fetch_sub(1, Ordering::Relaxed);
-                Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
+                Ok::<(), OpaqueError>(())
             });
         }
         // TODO: Add shutdown routine
         #[allow(unreachable_code)]
-        Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
+        Ok::<(), OpaqueError>(())
     });
 
     let subrequest_task = tokio::spawn(async move {
@@ -89,8 +78,8 @@ pub async fn get_input_tasks(
         }
         // TODO: Add shutdown routine
         #[allow(unreachable_code)]
-        Ok::<_, Box<dyn std::error::Error + Send + Sync>>(())
+        Ok::<_, OpaqueError>(())
     });
 
-    Ok::<_, Box<dyn std::error::Error + Send + Sync>>((main_listener_task, subrequest_task))
+    Ok::<_, OpaqueError>((main_listener_task, subrequest_task))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use futures::{future::select_all, future::FutureExt};
 use rustle::get_input_tasks;
 use rustle::QueryService;
+use socket2::{Domain, Protocol, Socket, Type};
+use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::sync::Arc;
 use structopt::StructOpt;
@@ -10,23 +13,16 @@ struct Opt {
     #[structopt(default_value = "8080", short, long)]
     port: i32,
 
-    #[structopt(default_value = "3", short, long)]
-    maxqueuesize: i32,
-
     #[structopt(default_value = "2001:558:feed::1:53", short, long)]
     router_addr: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // need to pass port and maxqueuesize to task instantiation
-    let Opt {
-        port,
-        maxqueuesize,
-        router_addr,
-    } = Opt::from_args();
-    let addr = format!("[::]:{}", port);
-    let socket = Arc::new(UdpSocket::bind(addr).await?);
+    let Opt { port, router_addr } = Opt::from_args();
+
+    let main_addr = format!("[::]:{}", port);
+    let sub_addr = "[::]:0";
 
     tokio::fs::create_dir_all("var/db").await?;
     tokio::fs::write("var/db/init.txt", "/something/something/").await?;
@@ -35,13 +31,48 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .index_db()
         .await?
         .register_for_periodic_update()?;
-    let update_task_handle = query_service.gib_update_task_handle().unwrap();
-    let (main_listener_task, subrequest_task) =
-        get_input_tasks(maxqueuesize, socket.clone(), router_addr, query_service).await?;
+    let update_task_handle = query_service
+        .gib_update_task_handle()
+        .ok_or("Update task handle is None")?;
+
+    let cpu_num = num_cpus::get();
+    let query_service = Arc::new(query_service);
+    let mut main_listener_tasks = Vec::new();
+    let mut subrequest_tasks = Vec::new();
+
+    println!("Starting here");
+    for _ in 0..cpu_num {
+        let (main_socket, sub_socket) = {
+            let main_socket = Socket::new(Domain::IPV6, Type::DGRAM, Some(Protocol::UDP))?;
+            main_socket.set_reuse_port(true)?;
+            main_socket.bind(&main_addr.parse::<SocketAddr>()?.into())?;
+
+            let sub_socket = Socket::new(Domain::IPV6, Type::DGRAM, Some(Protocol::UDP))?;
+            sub_socket.set_reuse_port(true)?;
+            sub_socket.bind(&sub_addr.parse::<SocketAddr>()?.into())?;
+            (
+                UdpSocket::from_std(main_socket.into())?,
+                UdpSocket::from_std(sub_socket.into())?,
+            )
+        };
+
+        let (main_listener_task, subrequest_task) = get_input_tasks(
+            main_socket.into(),
+            sub_socket.into(),
+            &router_addr,
+            query_service.clone(),
+        )
+        .await?;
+        main_listener_tasks.push(main_listener_task);
+        subrequest_tasks.push(subrequest_task);
+    }
+
+    let main_listener_tasks = select_all(main_listener_tasks).fuse();
+    let subrequest_tasks = select_all(subrequest_tasks).fuse();
 
     tokio::select! {
-        _ = main_listener_task => {}
-        _ = subrequest_task => {}
+        _ = main_listener_tasks => {}
+        _ = subrequest_tasks => {}
         update_res = update_task_handle => {
             match update_res {
                 Ok(_) => println!("Update task exited normally"),
